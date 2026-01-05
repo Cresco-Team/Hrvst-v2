ec9fa423c7e769aff51250e93a485b0dc618d096
Jerdon07 <jdlitaoen@gmail.com>
2026-01-05 12:00:00 +0800

    REFACTOR: Use native php support for role model's fillable

--- app/Models/Profiles/Role.php
+++ app/Models/Profiles/Role.php
@@ -3,13 +3,13 @@
 namespace App\Models\Profiles;
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+#[Fillable(['name'])]
 class Role extends Model
 {
-    protected $fillable = ['name'];
-
     public function users(): BelongsToMany
     {
         return $this->belongsToMany(User::class);
