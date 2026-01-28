6799d97ad70b8642f221285e6c9b62ec40311190
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-01-28 19:04:50 +0800

    FEAT: Create Dealer and Farmer profiles

---
+++ app/Models/Profiles/FarmerProfile.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Profiles;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FarmerProfile extends Model
+{
+    /** @use HasFactory<\Database\Factories\Profiles\FarmerProfileFactory> */
+    use HasFactory;
+}
