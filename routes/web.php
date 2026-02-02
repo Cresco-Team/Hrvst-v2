5bda099dc3a42e5c91443fa984997c6f42811761
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-02-02 16:45:35 +0800

    FEAT: Implement admin routing middleware. Implement auto admin signin. Require admin.php route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
@@ -10,8 +11,31 @@ Route::get('/', function () {
     ]);
 })->name('home');
 
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
+
+        if ($user->hasRole('admin')) return redirect()->route('admin.dashboard.index');
+
+        return Inertia::render('Dashboard');
+    });
+});
+
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+/* Development only */
+if (app()->environment('local', 'development')) {
+    Route::prefix('dev')->name('dev.')->group(function () {
+        Route::get('login/admin', function () {
+            Auth::loginUsingId(1);
+
+            return redirect()->route('admin.dashboard.index');
+        })->name('login.admin');
+    });
+}
+
 require __DIR__.'/settings.php';
+require __DIR__.'/admin.php';
