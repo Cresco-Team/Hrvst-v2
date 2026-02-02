71cd1db344b0e918e9b8d05c2af9debd55d97f11
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-02-02 10:24:25 +0800

    FEAT: Create AdminSeeder and register in database seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Profiles\Role;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $adminRole = Role::firstOrCreate([
+            'name' => 'admin'
+        ]);
+
+        $user = User::firstOrCreate([
+            'email' => 'admin@hrvst.com',
+            'phone_number' => '09303997215'
+        ], [
+            'name' => 'Admin Joe',
+            'password' => Hash::make('password'),
+            'isApproved' => true,
+        ]);
+
+        $user->roles()->syncWithoutDetaching([$adminRole->id]);
+    }
+}
