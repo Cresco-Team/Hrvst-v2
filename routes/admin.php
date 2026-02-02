32cc721c6059e83e48e7e4dd48b102e098cf6313
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-02-02 16:43:04 +0800

    FEAT: Add admin route file with its dashboard route

---
+++ routes/admin.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\Admin\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth', 'verified', 'admin'])->prefix('admin')->name('admin.')->group(function () {
+    
+    Route::get('/dashboard', [DashboardController::class, 'index'])
+        ->name('dashboard.index');
+});
