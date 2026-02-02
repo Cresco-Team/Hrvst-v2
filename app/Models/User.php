58b675e2f6e903a6937b835446ca589fbdafd6ed
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-02-02 10:17:07 +0800

    FEAT: Add phone_nummber and isApproved in fillable. Create relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,32 +3,29 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Models\Profiles\DealerProfile;
+use App\Models\Profiles\FarmerProfile;
+use App\Models\Profiles\Role;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, TwoFactorAuthenticatable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'phone_number',
+        'isApproved'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'two_factor_secret',
@@ -36,11 +33,6 @@ class User extends Authenticatable
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
@@ -48,5 +40,22 @@ class User extends Authenticatable
             'password' => 'hashed',
             'two_factor_confirmed_at' => 'datetime',
         ];
+    }
+
+    /* ---------- relations ---------- */
+
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function farmerProfile(): HasOne
+    {
+        return $this->hasOne(FarmerProfile::class);
+    }
+
+    public function dealerProfile(): HasOne
+    {
+        return $this->hasOne(DealerProfile::class);
     }
 }
