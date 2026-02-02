7a37edb04ce4841bb9ddf12e4968d8b9259e57b5
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-02-02 19:11:25 +0800

    REFACTOR: Remove the index method call in the dashboard controller route in admin route

--- routes/admin.php
+++ routes/admin.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified', 'admin'])->prefix('admin')->name('admin.')->group(function () {
     
-    Route::get('/dashboard', [DashboardController::class, 'index'])
-        ->name('dashboard.index');
+    Route::get('/dashboard', DashboardController::class)
+        ->name('dashboard');
 });
