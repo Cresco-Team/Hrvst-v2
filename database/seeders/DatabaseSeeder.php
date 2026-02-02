b1eaa7201c61b705d6446c74d05b663b60cb06bf
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-02-02 19:09:29 +0800

    REFACTOR: Register variety and price history seeder in the database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Database\Seeders\Product\PriceHistorySeeder;
+use Database\Seeders\Product\VarietySeeder;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -11,6 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            VarietySeeder::class,
+            PriceHistorySeeder::class,
             AdminSeeder::class,
         ]);
     }
