aff70cfc0f905df180889c52895aeba98ecbd071
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-02-02 17:35:50 +0800

    REFACTOR: Add fillable and user relation

--- app/Models/Profiles/DealerProfile.php
+++ app/Models/Profiles/DealerProfile.php
@@ -2,10 +2,25 @@
 
 namespace App\Models\Profiles;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DealerProfile extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'is_approved',
+        'document_image',
+    ];
+
+    /* ---------- relations ---------- */
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
