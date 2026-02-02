ad59e7cdd0ade5fec5fc34cf43e0705205e61193
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-02-02 17:36:11 +0800

    REFACTOR: Add relations to farmer profile model

--- app/Models/Profiles/FarmerProfile.php
+++ app/Models/Profiles/FarmerProfile.php
@@ -2,9 +2,14 @@
 
 namespace App\Models\Profiles;
 
+use App\Models\Address\Barangay;
+use App\Models\Address\Municipality;
+use App\Models\Address\Province;
 use App\Models\Product\Planting;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class FarmerProfile extends Model
@@ -14,6 +19,26 @@ class FarmerProfile extends Model
     protected $fillable = [];
 
     /* ---------- relations ---------- */
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function province(): BelongsTo
+    {
+        return $this->belongsTo(Province::class);
+    }
+
+    public function municipality(): BelongsTo
+    {
+        return $this->belongsTo(Municipality::class);
+    }
+
+    public function barangay(): BelongsTo
+    {
+        return $this->belongsTo(Barangay::class);
+    }
 
     public function plantings(): HasMany
     {
