cefa390200ca249a264228b75f2f843f0e6ce5dc
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-02-04 08:50:01 +0800

    FEA: Use kebab-case in the route for SEO and snake_case in the name for javascript readabbility

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\VegetableController;
+use App\Http\Controllers\Admin\VarietyController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified', 'admin'])->prefix('admin')->name('admin.')->group(function () {
@@ -9,6 +9,12 @@ Route::middleware(['auth', 'verified', 'admin'])->prefix('admin')->name('admin.'
     Route::get('/dashboard', DashboardController::class)
         ->name('dashboard');
 
-    Route::get('/vegetables', [VegetableController::class, 'index'])
-        ->name('vegetables.index');
+    Route::get('/vegetables-varieties', [VarietyController::class, 'index'])
+        ->name('vegetables_varieties.index');
+    Route::post('/vegetables_varieties', [VarietyController::class, 'store'])
+        ->name('vegetables_varieties.store');
+    Route::put('/vegetables-varieties/{variety}', [VarietyController::class, 'update'])
+        ->name('vegetables_varieties.update');
+    Route::delete('/vegetables-varieties/{variety}', [VarietyController::class, 'destroy'])
+        ->name('vegetables_varieties.destroy');
 });
