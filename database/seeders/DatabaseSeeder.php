5874a55ddd9dd7594b48dafb56a4802b3b7eaf5f
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-02-05 00:10:03 +0800

    FEAT: Registers all the necessare seeders in the database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,13 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Database\Seeders\Address\BarangaySeeder;
+use Database\Seeders\Address\MunicipalitySeeder;
+use Database\Seeders\Address\ProvinceSeeder;
 use Database\Seeders\Product\PriceHistorySeeder;
 use Database\Seeders\Product\VarietySeeder;
+use Database\Seeders\Profiles\AdminSeeder;
+use Database\Seeders\Profiles\FarmerSeeder;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,9 +18,15 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            ProvinceSeeder::class,
+            MunicipalitySeeder::class,
+            BarangaySeeder::class,
+
             VarietySeeder::class,
             PriceHistorySeeder::class,
+            
             AdminSeeder::class,
+            FarmerSeeder::class,
         ]);
     }
 }
