9d41a0edcaccae3c0859fd3ea7f1f6e06323cab9
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-02-08 22:27:43 +0800

    FEAT: Add requests routes for dealer

--- routes/dealer.php
+++ routes/dealer.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Dealer\DealerRequestController;
 use App\Http\Controllers\Dealer\MarketController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,4 +9,12 @@ Route::middleware(['auth', 'verified', 'dealer'])->prefix('dealer')->name('deale
     // Market Dashboard
     Route::get('/market', [MarketController::class, 'index'])
         ->name('market');
+
+    Route::prefix('requests')->name('requests.')->group(function () {
+        Route::get('/', [DealerRequestController::class, 'index'])->name('index');
+        Route::post('/', [DealerRequestController::class, 'store'])->name('store');
+        Route::put('/{dealerRequest}', [DealerRequestController::class, 'update'])->name('update');
+        Route::post('/{dealerRequest}/fulfill', [DealerRequestController::class, 'fulfill'])->name('fulfill');
+        Route::delete('/{dealerRequest}', [DealerRequestController::class, 'destroy'])->name('destroy');
+    });
 });
