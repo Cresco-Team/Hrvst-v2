091d6920ec76267ebe64f0b733ee4e354592014b
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-02-11 08:32:08 +0800

    FEAT: Update large card to include icon component

--- resources/js/components/shared/cards/LargeCard.vue
+++ resources/js/components/shared/cards/LargeCard.vue
@@ -5,38 +5,32 @@ import CardContent from '@/components/ui/card/CardContent.vue';
 import CardDescription from '@/components/ui/card/CardDescription.vue';
 import CardHeader from '@/components/ui/card/CardHeader.vue';
 import CardTitle from '@/components/ui/card/CardTitle.vue';
+import type { Component } from 'vue';
 
-const props = defineProps({
-    title: {
-        type: String,
-        required: true,
-        default: 'Card Title',
-    },value: {
-        tyle: Number,
-        required: true,
-        default: 0,
-    }, subtext: {
-        type: String,
-        required: false,
-        default: '',
-    }, change: {
-        type: String,
-        required: false,
-        default: 0,
-    }, icon: {
-        type: Object,
-        required: false,
-        default: null,
-    }, trendColor: {
-        type: String,
-        required: false,
-        default: null,
-    }, cardClass: {
-        type: String,
-        required: false,
-        default: null,
+const props = withDefaults(
+    defineProps<{
+        title: string;
+        value: number;
+        subtext?: string;
+        change?: string;
+        badge?: Record<string, any> | null;
+        trendColor?: string | null;
+        cardClass?: string | null;
+        icon?: Component;
+        iconColor?: string;
+    }>(),
+    {
+        title: 'Card Title',
+        value: 0,
+        subtext: '',
+        change: '',
+        badge: null,
+        trendColor: null,
+        cardClass: null,
+        icon: undefined,
+        iconColor: 'text-primary',
     }
-})
+)
 </script>
 
 <template>
@@ -44,9 +38,18 @@ const props = defineProps({
         <CardContent class="flex items-center justify-between">
             <CardDescription>{{ title }}</CardDescription>
             <Badge v-if="change" variant="outline">
-                <component :is="icon" :class="trendColor"/>
+                <component 
+                    :is="badge" 
+                    :class="trendColor" 
+                />
                 <span :class="trendColor">{{ change }}</span>
             </Badge>
+            <component 
+                v-else-if="icon" 
+                :is="icon" 
+                :class="iconColor" 
+            />
+
         </CardContent>
         <CardHeader>
             <CardTitle class="text-5xl">
