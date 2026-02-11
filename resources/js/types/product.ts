fa61ec024d848d2316910ee6c49e565d65b3f1a9
Jerdon07 <jdlitaoen@gmail.com>
2026-02-11 11:44:09 +0800

    FEAT: Create type interface for product props

---
+++ resources/js/types/product.ts
@@ -0,0 +1,30 @@
+
+export interface Category {
+    id: number
+    name: string
+}
+
+export interface Vegetable {
+    id: number
+    name: string
+    category: Category
+}
+
+export interface Variety {
+    id: number
+    name: string
+    image_path: string
+    weeks_to_harvest: number
+    vegetable: Vegetable
+}
+
+export interface Planting {
+    id: number
+    variety: Variety
+    weight_kg: number
+    date_planted: string
+    expected_harvest_date: string
+    days_until_harvest: number | null
+    status_badge: string
+}
+
