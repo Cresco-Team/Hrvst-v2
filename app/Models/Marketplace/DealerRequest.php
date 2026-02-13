2beccbf5a3e68e56ae3fa6b690e97350660cef04
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-02-13 22:37:57 +0800

    FIX: Fix the namespace

--- app/Models/Marketplace/DealerRequest.php
+++ app/Models/Marketplace/DealerRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models\Announcement;
+namespace App\Models\Marketplace;
 
 use App\DealerPriceFlag;
 use App\DealerRequestStatus;
@@ -12,7 +12,6 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 class DealerRequest extends Model
