9190ac3234e85b728f867c936f04cb45c22623fb
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-02-13 19:26:45 +0800

    fwevdsvsdv

--- app/Models/Profiles/DealerProfile.php
+++ app/Models/Profiles/DealerProfile.php
@@ -18,9 +18,12 @@ class DealerProfile extends Model
         'document_image',
     ];
 
-    protected $casts = [
-        'is_approved' => 'boolean',
-    ];
+    protected function casts(): array
+    {
+        return [
+            'is_approved' => 'boolean',
+        ];
+    }
 
     /* ---------- relations ---------- */
 
