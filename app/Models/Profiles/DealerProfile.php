36f13cefdc3c6d1e1fa5cd67ef07aa0f5857e83e
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-02-14 20:38:58 +0800

    FIX: Fix the dealer request importation

--- app/Models/Profiles/DealerProfile.php
+++ app/Models/Profiles/DealerProfile.php
@@ -2,7 +2,7 @@
 
 namespace App\Models\Profiles;
 
-use App\Models\Announcement\DealerRequest;
+use App\Models\Marketplace\DealerRequest;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
