3cd256bb73191ae192369780f818d46998cd9a8d
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-02-16 17:28:56 +0800

    FEAT: Create shared EmptyState component

---
+++ resources/js/components/EmptyState.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { Component } from 'vue';
+import { Button } from './ui/button';
+import { Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from './ui/empty';
+
+const props = defineProps<{
+        title: string
+        description: string
+        icon: Component
+        button: string
+}>()
+</script>
+
+<template>
+    <Empty class="border border-dashed mx-20">
+        <EmptyHeader>
+            <EmptyMedia variant="icon">
+                <component :is="icon" />
+            </EmptyMedia>
+            <EmptyTitle>{{ title }}</EmptyTitle>
+            <EmptyDescription>
+                {{ description }}
+            </EmptyDescription>
+        </EmptyHeader>
+        <EmptyContent>
+            <Button size="sm" >
+                {{ button }}
+            </Button>
+        </EmptyContent>
+    </Empty>
+</template>
