f3bb29fb371031260df1da50de3cefc5194533ee
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-02-16 17:30:20 +0800

    FEAT: Create garden routes fro the farmer

--- routes/farmer.php
+++ routes/farmer.php
@@ -1,13 +1,17 @@
 <?php
 
 use App\Http\Controllers\Farmer\DealerRequestBrowseController;
-use App\Http\Controllers\Farmer\FarmerOfferingController;
+use App\Http\Controllers\Farmer\OfferingController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified', 'farmer'])->prefix('farmer')->name('farmer.')->group(function () {
     
     Route::prefix('garden')->name('garden.')->group(function () {
         Route::get('/', [OfferingController::class, 'index'])->name('index');
+        Route::post('/', [OfferingController::class, 'store'])->name('store');
+        Route::put('/{offering}', [OfferingController::class, 'update'])->name('update');
+        Route::post('/{offering}/archive', [OfferingController::class, 'archive'])->name('archive');
+        Route::delete('/{offering}', [OfferingController::class, 'destroy'])->name('destroy');
     });
 
     // Garden (Plantings Dashboard)
