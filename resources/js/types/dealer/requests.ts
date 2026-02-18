7a0b212c0baa88c74176eee8e015084fc85f7c1b
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-02-18 20:09:53 +0800

    FEAT: Create dealer request type file

---
+++ resources/js/types/dealer/requests.ts
@@ -0,0 +1,37 @@
+
+export interface Summary {
+    total_open: number
+    total_fulfilled: number
+    total_expired: number
+    upcomming_transactions: number
+}
+
+export interface VarietyOption {
+    id: number
+    name: string
+    current_price: {
+        min: number
+        max: number
+    }
+}
+
+export interface Request {
+    id: number
+    dealer: {
+        id: number
+        name: string
+    }
+    variety: {
+        id: number
+        name: string
+        vegetable: string
+        image_url: string
+    }
+    quantity_kg: number
+    price_offered: number
+    transaction_date: string
+    days_until_transaction: number
+    status: string
+    created_at_human: string
+}
+
