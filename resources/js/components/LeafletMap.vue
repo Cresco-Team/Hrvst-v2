5fbaa45d94ecfeed4afa874070b686dc4cb43de1
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-02-19 11:27:07 +0800

    FEAT: Create declarative component built on top of useLeafletMap

---
+++ resources/js/components/LeafletMap.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { watch, nextTick } from 'vue'
+import { useLeafletMap, type MapMarker } from '@/composables/useLeafletMap'
+
+type Props = {
+  lat: number
+  lng: number
+  zoom?: number
+  markers?: MapMarker[]
+  class?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  zoom: 14,
+  markers: () => [],
+})
+
+const { container, init, destroy } = useLeafletMap({ zoom: props.zoom })
+
+watch(
+  () => [props.lat, props.lng] as const,
+  async ([lat, lng]) => {
+    await nextTick()
+    await init(lat, lng, props.markers)
+  },
+  { immediate: true },
+)
+
+watch(
+  () => props.markers,
+  async () => {
+    await nextTick()
+    await init(props.lat, props.lng, props.markers)
+  },
+  { deep: true },
+)
+
+defineExpose({ destroy })
+</script>
+
+<template>
+  <div ref="container" :class="['h-48 w-full overflow-hidden rounded-md border', props.class]" />
+</template>
