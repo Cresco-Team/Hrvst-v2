32244edde073f1498e26d25c0478d19e00de9aa3
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-02-19 23:00:28 +0800

    FIX: Rename the drop table to dealer_demand

--- database/migrations/2026_02_08_120536_create_dealer_demands_table.php
+++ database/migrations/2026_02_08_120536_create_dealer_demands_table.php
@@ -28,6 +28,6 @@ return new class extends Migration
 
     public function down(): void
     {
-        Schema::dropIfExists('dealer_requests');
+        Schema::dropIfExists('dealer_demands');
     }
 };
