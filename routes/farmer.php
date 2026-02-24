cb8476e6a8608a859b121b203075f49039901536
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-02-24 16:59:05 +0800

    FIX: Fix the farmer route garden

--- routes/farmer.php
+++ routes/farmer.php
@@ -1,17 +1,17 @@
 <?php
 
 use App\Http\Controllers\Farmer\MarketplaceController;
-use App\Http\Controllers\Farmer\OfferingController;
+use App\Http\Controllers\Farmer\SupplyController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified', 'farmer'])->prefix('farmer')->name('farmer.')->group(function () {
     
     Route::prefix('garden')->name('garden.')->group(function () {
-        Route::get('/', [OfferingController::class, 'index'])->name('index');
-        Route::post('/', [OfferingController::class, 'store'])->name('store');
-        Route::put('/{offering}', [OfferingController::class, 'update'])->name('update');
-        Route::post('/{offering}/archive', [OfferingController::class, 'archive'])->name('archive');
-        Route::delete('/{offering}', [OfferingController::class, 'destroy'])->name('destroy');
+        Route::get('/', [SupplyController::class, 'index'])->name('index');
+        Route::post('/', [SupplyController::class, 'store'])->name('store');
+        Route::put('/{offering}', [SupplyController::class, 'update'])->name('update');
+        Route::post('/{offering}/archive', [SupplyController::class, 'archive'])->name('archive');
+        Route::delete('/{offering}', [SupplyController::class, 'destroy'])->name('destroy');
     });
 
     Route::prefix('marketplace')->name('marketplace.')->group(function () {
