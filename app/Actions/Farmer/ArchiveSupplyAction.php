ebdeffee0a438ebf001700050542c7e3839bb08f
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-02-25 21:56:18 +0800

    REFACTOR: Use method from Post model

--- app/Actions/Farmer/ArchiveSupplyAction.php
+++ app/Actions/Farmer/ArchiveSupplyAction.php
@@ -2,13 +2,12 @@
 
 namespace App\Actions\Farmer;
 
-use App\Enums\PostStatus;
 use App\Models\Marketplace\FarmerSupply;
 
 final class ArchiveSupplyAction
 {
-    public function execute(FarmerSupply $supply): void
+    public function __invoke(FarmerSupply $supply): void
     {
-        $supply->post->update(['status' => PostStatus::Archived]);
+        $supply->post->markAsArchived();
     }
 }
--- app/Actions/Farmer/FulfilSupplyAction.php
+++ app/Actions/Farmer/FulfilSupplyAction.php
@@ -2,13 +2,12 @@
 
 namespace App\Actions\Farmer;
 
-use App\Enums\PostStatus;
 use App\Models\Marketplace\FarmerSupply;
 
 final class FulfillSupplyAction
 {
     public function execute(FarmerSupply $supply): void
     {
-        $supply->post->update(['status' => PostStatus::Fulfilled]);
+        $supply->post->markAsFulfilled();
     }
 }
