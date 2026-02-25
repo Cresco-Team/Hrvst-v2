8b04aeea007c0f546291ba7d37d907395d4627c9
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-02-25 21:54:16 +0800

    REFACTOR: Use supply as the wildcard

--- routes/farmer.php
+++ routes/farmer.php
@@ -4,14 +4,19 @@ use App\Http\Controllers\Farmer\MarketplaceController;
 use App\Http\Controllers\Farmer\SupplyController;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/farmer/garden-test', function () {
+    return response()->json(['hit' => true]);
+});
+
 Route::middleware(['auth', 'verified', 'farmer'])->prefix('farmer')->name('farmer.')->group(function () {
     
     Route::prefix('garden')->name('garden.')->group(function () {
         Route::get('/', [SupplyController::class, 'index'])->name('index');
         Route::post('/', [SupplyController::class, 'store'])->name('store');
-        Route::put('/{offering}', [SupplyController::class, 'update'])->name('update');
-        Route::post('/{offering}/archive', [SupplyController::class, 'archive'])->name('archive');
-        Route::delete('/{offering}', [SupplyController::class, 'destroy'])->name('destroy');
+        Route::put('/{supply}', [SupplyController::class, 'update'])->name('update');
+        Route::post('/{supply}/archive', [SupplyController::class, 'archive'])->name('archive');
+        Route::post('/{supply}/fulfill', [SupplyController::class, 'fulfill'])->name('fulfill');
+        Route::delete('/{supply}', [SupplyController::class, 'destroy'])->name('destroy');
     });
 
     Route::prefix('marketplace')->name('marketplace.')->group(function () {
