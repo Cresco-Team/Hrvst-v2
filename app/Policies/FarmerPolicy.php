754ead2466c0e4756b08a52436125e63c66aa64b
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-02-28 22:41:33 +0800

    REFACTOR: Rename FarmerProfilePolicy to FarmerPolicy and add gates

---
+++ app/Policies/FarmerPolicy.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Profiles\FarmerProfile;
+use App\Models\User;
+
+class FarmerPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return $user->hasRole('admin')
+            || $user->farmerProfile?->is_approved
+            || $user->dealerProfile?->is_approved
+        ;
+    }
+
+    public function view(User $user, FarmerProfile $farmerProfile): bool
+    {
+        return $this->viewAny($user);
+    }
+
+    public function update(User $user, FarmerProfile $farmerProfile): bool
+    {
+        return $user->hasRole('admin')
+            || $user->farmerProfile?->id === $farmerProfile->id;
+    }
+
+    public function approve(User $user): bool
+    {
+        return $user->hasRole('admin');
+    }
+
+    public function reject(User $user): bool
+    {
+        return $user->hasRole('admin');
+    }
+
+    public function delete(User $user, FarmerProfile $farmerProfile): bool
+    {
+        return $this->update($user, $farmerProfile);
+    }
+
+    public function restore(User $user, FarmerProfile $farmerProfile): bool
+    {
+        return $user->hasRole('admin');
+    }
+
+    public function forceDelete(User $user, FarmerProfile $farmerProfile): bool
+    {
+        return $this->update($user, $farmerProfile);
+    }
+}
