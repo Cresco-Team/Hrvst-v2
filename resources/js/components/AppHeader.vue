5511f6fbec63de0eb4d2d0a716f7e314190aadf3
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-03-05 10:20:52 +0800

    FEAT: Add the new vegetable pages of farmer and dealer

--- resources/js/components/AppHeader.vue
+++ resources/js/components/AppHeader.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 
 import { Link, usePage } from '@inertiajs/vue3';
-import { Gem, Menu, MessagesSquare, PackageSearch, Search, Sprout, Store, Truck } from 'lucide-vue-next';
+import { Gem, Menu, MessagesSquare, Package, PackageSearch, Search, Sprout, Store, Truck } from 'lucide-vue-next';
 import { computed } from 'vue';
 import AppLogo from '@/components/AppLogo.vue';
 import AppLogoIcon from '@/components/AppLogoIcon.vue';
@@ -62,11 +62,15 @@ const mainNavItems = computed<NavItem[]>(() => {
 
         if (page.props.auth.user.roles.includes('dealer')) {
             items.push({
+                title: 'Vegetables',
+                href: dealer.vegetables.index(),
+                icon: Sprout
+            }, {
                 title: 'Marketplace',
                 href: dealer.marketplace.index(),
                 icon: Store
             }, {
-                title: 'My Requests',
+                title: 'My Posts',
                 href: dealer.demands.index(),
                 icon: PackageSearch
             })
@@ -74,13 +78,17 @@ const mainNavItems = computed<NavItem[]>(() => {
 
         if (page.props.auth.user.roles.includes('farmer')) {
             items.push({
+                title: 'Vegetables',
+                href: farmer.vegetables.index(),
+                icon: Sprout
+            }, {
                 title: 'Marketplace',
                 href: farmer.marketplace.index(),
                 icon: Truck
             }, {
                 title: 'My Garden',
                 href: farmer.garden.index(),
-                icon: Sprout
+                icon: Package
             })
         }
     
