376ba017218f14291c80f8c883410c654bbe2570
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-03-05 18:39:54 +0800

    FEAT: Add flash message to app page props

--- resources/js/types/index.ts
+++ resources/js/types/index.ts
@@ -1,14 +1,24 @@
-export * from './auth';
-export * from './navigation';
-export * from './ui';
+export * from './auth'
+export * from './navigation'
+export * from './ui'
 
-import type { Auth } from './auth';
+import type { Auth } from './auth'
+
+export type FlashType = 'success' | 'error' | 'warning' | 'info'
+
+export type FlashMessage = {
+  success: string | null
+  error: string | null
+  warning: string | null
+  info: string | null
+}
 
 export type AppPageProps<
-    T extends Record<string, unknown> = Record<string, unknown>,
+  T extends Record<string, unknown> = Record<string, unknown>,
 > = T & {
-    name: string;
-    auth: Auth;
-    sidebarOpen: boolean;
-    [key: string]: unknown;
-};
+  name: string
+  auth: Auth
+  sidebarOpen: boolean
+  flash: FlashMessage
+  [key: string]: unknown
+}
