3fe0a427a1173893762e80bf0a5fd5c3aa84ef98
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-03-07 07:56:36 +0800

    FIX: Fix the categoryOptions props

--- app/Http/Controllers/Admin/VegetableController.php
+++ app/Http/Controllers/Admin/VegetableController.php
@@ -28,7 +28,7 @@ class VegetableController extends Controller
                 perPage: 20,
                 priceFilter: $request->query('price_filter', null)
             )),
-            'vegetableOptions' => Inertia::defer(fn () => VarietyService::vegetableOptions()),
+            'categoryOptions' => Inertia::defer(fn () => VarietyService::categoryOptions()),
         ]);
     }
 
