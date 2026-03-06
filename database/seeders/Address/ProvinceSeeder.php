a5e9f9c4519d862ddc9fbb7753a0aab1ef113c40
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-03-06 22:44:37 +0800

    REFACTOR: Replace primary key with name based forstOrCreate

--- database/seeders/Address/ProvinceSeeder.php
+++ database/seeders/Address/ProvinceSeeder.php
@@ -3,16 +3,12 @@
 namespace Database\Seeders\Address;
 
 use App\Models\Address\Province;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class ProvinceSeeder extends Seeder
 {
     public function run(): void
     {
-        Province::updateOrCreate(
-            ['id' => 1],
-            ['name' => 'Benguet']
-        );
+        Province::firstOrCreate(['name' => 'Benguet']);
     }
 }
