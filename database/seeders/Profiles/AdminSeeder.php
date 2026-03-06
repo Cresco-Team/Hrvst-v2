7788f057d77d869517bbb54db8b047f5fda7fc89
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-03-06 22:47:54 +0800

    REFACTOR: Add email_verified_at

--- database/seeders/Profiles/AdminSeeder.php
+++ database/seeders/Profiles/AdminSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders\Profiles;
 
 use App\Models\Profiles\Role;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -12,18 +11,20 @@ class AdminSeeder extends Seeder
 {
     public function run(): void
     {
-        $adminRole = Role::firstOrCreate([
-            'name' => 'admin'
-        ]);
+        $adminRole = Role::firstOrCreate(['name' => 'admin']);
 
-        $user = User::firstOrCreate([
-            'email' => 'admin@hrvst.com',
-            'phone_number' => '09303997215'
-        ], [
-            'name' => 'Admin Joe',
-            'password' => Hash::make('password'),
-        ]);
+        $user = User::firstOrCreate(
+            ['email' => 'admin@hrvst.com'],
+            [
+                'name'              => 'Admin Joe',
+                'phone_number'      => '09303997215',
+                'email_verified_at' => now(),
+                'password'          => Hash::make('password'),
+            ]
+        );
 
         $user->roles()->syncWithoutDetaching([$adminRole->id]);
+
+        $this->command->info('✓ Admin seeded: admin@hrvst.com / password');
     }
 }
