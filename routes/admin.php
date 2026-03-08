5e854066ae08a4ea0779d3abd11ae4bb1014f432
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-03-08 19:54:19 +0800

    FEAT: Implement prices creation in the backend

--- routes/admin.php
+++ routes/admin.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\DealerController;
 use App\Http\Controllers\Admin\FarmerController;
 use App\Http\Controllers\Admin\FlagController;
+use App\Http\Controllers\Admin\VarietyPriceController;
 use App\Http\Controllers\Admin\VegetableController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,7 @@ Route::middleware(['auth', 'verified', 'admin'])->prefix('admin')->name('admin.'
         Route::get('/{variety}/details', [VegetableController::class, 'details'])->name('details');
         Route::post('/', [VegetableController::class, 'store'])->name('store');
         Route::put('/{variety}', [VegetableController::class, 'update'])->name('update');
+        Route::post('/{variety}/prices', VarietyPriceController::class, 'store')->name('prices.store');
         Route::delete('/{variety}', [VegetableController::class, 'destroy'])->name('destroy');
     });
 
