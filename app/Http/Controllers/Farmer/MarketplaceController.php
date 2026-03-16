a64b72c054512bf2e3b5336dc3edf1eb3ca703f3
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-03-16 18:24:31 +0800

    REFACTOR: Remove unused imports

--- app/Http/Controllers/Farmer/MarketplaceController.php
+++ app/Http/Controllers/Farmer/MarketplaceController.php
@@ -4,10 +4,8 @@ namespace App\Http\Controllers\Farmer;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Marketplace\DealerDemandResource;
-use App\Models\Marketplace\DealerDemand;
 use App\Services\Farmer\MarketplaceService;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Gate;
 use Inertia\Inertia;
 use Inertia\Response;
 
