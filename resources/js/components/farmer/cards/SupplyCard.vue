a8214a7726ded38d39274353ccae1a9e8dd34971
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-03-18 21:12:40 +0800

    FEAT: Add some supply details

--- resources/js/components/farmer/cards/SupplyCard.vue
+++ resources/js/components/farmer/cards/SupplyCard.vue
@@ -1,41 +1,41 @@
 <script setup lang="ts">
 import {
-	Archive,
-	CalendarClock,
-	MoreVertical,
-	PackageCheck,
-	Pencil,
-	PhilippinePeso,
-	SquareEqual,
-	Trash,
-	Weight,
+    Archive,
+    CalendarClock,
+    MoreVertical,
+    PackageCheck,
+    Pencil,
+    PhilippinePeso,
+    SquareEqual,
+    Trash,
+    Weight,
 } from 'lucide-vue-next'
 import { computed } from 'vue'
 import { AspectRatio } from '@/components/ui/aspect-ratio'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuSeparator,
-	DropdownMenuTrigger,
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 import { Separator } from '@/components/ui/separator'
 import type { Supply, VarietyOption } from '@/types/marketplace'
 
 interface Props {
-	supply: Supply
-	varietyOptions?: Record<string, VarietyOption[]>
+    supply: Supply
+    varietyOptions?: Record<string, VarietyOption[]>
 }
 
 const props = defineProps<Props>()
 
 const emit = defineEmits<{
-	edit: [supply: Supply]
-	archive: [supply: Supply]
-	fulfill: [supply: Supply]
-	delete: [supply: Supply]
+    edit: [supply: Supply]
+    archive: [supply: Supply]
+    fulfill: [supply: Supply]
+    delete: [supply: Supply]
 }>()
 
 const isOngoing = computed(() => props.supply.status === 'Ongoing')
@@ -45,14 +45,11 @@ const isFulfilled = computed(() => props.supply.status === 'Fulfilled')
 
 <template>
     <Card class="py-0 gap-2 overflow-hidden transition-all hover:shadow-lg ">
-        <AspectRatio :ratio="16/9" class="relative overflow-hidden">
-            <img 
-                v-if="supply.image_url"
-                :src="supply.image_url"
-                :alt="supply.variety.name.charAt(0)" 
-                class="size-full object-cover bg-gray-200"
-            />
-            <div class="absolute bottom-0 right-0 rounded-tl-lg bg-black/60 px-3 py-1 text-xs font-medium text-white backdrop-blur-sm">
+        <AspectRatio :ratio="16 / 9" class="relative overflow-hidden">
+            <img v-if="supply.image_url" :src="supply.image_url" :alt="supply.variety.name.charAt(0)"
+                class="size-full object-cover bg-gray-200" />
+            <div
+                class="absolute bottom-0 right-0 rounded-tl-lg bg-black/60 px-3 py-1 text-xs font-medium text-white backdrop-blur-sm">
                 {{ supply.created_at_human }}
             </div>
 
@@ -64,38 +61,26 @@ const isFulfilled = computed(() => props.supply.status === 'Fulfilled')
                 </DropdownMenuTrigger>
 
                 <DropdownMenuContent align="end">
-                    <DropdownMenuItem
-                        v-if="isOngoing"
-                        @click="emit('edit', supply)"
-                    >
+                    <DropdownMenuItem v-if="isOngoing" @click="emit('edit', supply)">
                         <Pencil class="mr-2 size-4" />
                         Edit Details
                     </DropdownMenuItem>
 
-                    <DropdownMenuSeparator v-if="isOngoing"/>
-                    
-                    <DropdownMenuItem
-                        v-if="isOngoing || isFulfilled"
-                        @click="emit('archive', supply)"
-                        class="text-orange-600 dark:text-orange-400"
-                    >
+                    <DropdownMenuSeparator v-if="isOngoing" />
+
+                    <DropdownMenuItem v-if="isOngoing || isFulfilled" @click="emit('archive', supply)"
+                        class="text-orange-600 dark:text-orange-400">
                         <Archive class="mr-2 size-4" />
                         Archive
                     </DropdownMenuItem>
 
-                    <DropdownMenuItem
-                        v-if="isOngoing || isArchived"
-                        @click="emit('fulfill', supply)"
-                        class="text-green-500 dark:text-green-400"
-                    >
+                    <DropdownMenuItem v-if="isOngoing || isArchived" @click="emit('fulfill', supply)"
+                        class="text-green-500 dark:text-green-400">
                         <PackageCheck class="mr-2 size-4" />
                         Fulfill
                     </DropdownMenuItem>
 
-                    <DropdownMenuItem
-                        @click="emit('delete', supply)"
-                        class="text-destructive"
-                    >
+                    <DropdownMenuItem @click="emit('delete', supply)" class="text-destructive">
                         <Trash class="mr-2 size-4" />
                         Delete
                     </DropdownMenuItem>
@@ -104,7 +89,7 @@ const isFulfilled = computed(() => props.supply.status === 'Fulfilled')
         </AspectRatio>
 
         <CardHeader class="p-5 py-2">
-            <CardTitle>
+            <CardTitle class="line-clamp-1">
                 {{ supply.variety.vegetable }} {{ supply.variety.name }}
             </CardTitle>
             <CardDescription>
