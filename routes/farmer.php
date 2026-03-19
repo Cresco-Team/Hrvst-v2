031cc237cec6d1ae68c258abe2860fb274370c4c
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-03-19 13:32:02 +0800

    REFACTOR: Rename the farmer garden route to supplies

--- routes/farmer.php
+++ routes/farmer.php
@@ -6,15 +6,11 @@ use App\Http\Controllers\Farmer\SupplyMapController;
 use App\Http\Controllers\Farmer\VegetablesController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/farmer/garden-test', function () {
-    return response()->json(['hit' => true]);
-});
-
 Route::middleware(['auth', 'verified', 'farmer'])->prefix('farmer')->name('farmer.')->group(function () {
-    
+
     Route::get('/vegetables', [VegetablesController::class, 'index'])->name('vegetables.index');
 
-    Route::prefix('garden')->name('garden.')->group(function () {
+    Route::prefix('supplies')->name('supplies.')->group(function () {
         Route::get('/', [SupplyController::class, 'index'])->name('index');
         Route::post('/', [SupplyController::class, 'store'])->name('store');
         Route::put('/{supply}', [SupplyController::class, 'update'])->name('update');
@@ -22,9 +18,10 @@ Route::middleware(['auth', 'verified', 'farmer'])->prefix('farmer')->name('farme
         Route::post('/{supply}/fulfill', [SupplyController::class, 'fulfill'])->name('fulfill');
         Route::delete('/{supply}', [SupplyController::class, 'destroy'])->name('destroy');
     });
+
     Route::prefix('supply-map')->name('supply-map.')->group(function () {
         Route::get('/', [SupplyMapController::class, 'index'])->name('index');
-        Route::get('/api/markers', [SupplyMapController::class, 'markers'])->name('markers');        
+        Route::get('/api/markers', [SupplyMapController::class, 'markers'])->name('markers');
     });
 
     Route::prefix('marketplace')->name('marketplace.')->group(function () {
