47913cf862bd53c692ad8f2ca4f1bdf3ac351bae
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-03-19 09:18:48 +0800

    FIX: Change the demands to posts

--- app/Http/Resources/Profile/DealerResource.php
+++ app/Http/Resources/Profile/DealerResource.php
@@ -3,7 +3,6 @@
 namespace App\Http\Resources\Profile;
 
 use App\Http\Resources\Marketplace\DealerDemandResource;
-use App\Http\Resources\Profile\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -13,22 +12,20 @@ class DealerResource extends JsonResource
     {
         return [
             /* Always present */
-            'id'              => $this->id,
-            'is_approved'     => $this->is_approved,
-            'joined_at'       => $this->created_at->format('M d, Y'),
+            'id' => $this->id,
+            'is_approved' => $this->is_approved,
+            'joined_at' => $this->created_at->format('M d, Y'),
             'joined_at_human' => $this->created_at->diffForHumans(),
 
             /* with('user') or with('user.media') */
-            'user' => $this->whenLoaded('user', fn () =>
-                (new UserResource($this->user))->toArray($request)
+            'user' => $this->whenLoaded('user', fn () => (new UserResource($this->user))->toArray($request)
             ),
 
-            /* with('demands.*') */
-            'demands' => $this->whenLoaded('demands', fn () =>
-                $this->demands
-                    ->map(fn ($demand) => (new DealerDemandResource($demand))->toArray($request))
-                    ->values()
-                    ->all()
+            /* with('posts.*') */
+            'demands' => $this->whenLoaded('posts', fn () => $this->posts
+                ->map(fn ($demand) => (new DealerDemandResource($demand))->toArray($request))
+                ->values()
+                ->all()
             ),
 
             /* withCount(['demands as ongoing_demands_count' => ...]) */
