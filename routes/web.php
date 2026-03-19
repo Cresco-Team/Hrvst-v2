3a4ae4c53aa17af75d5a26446aea0793bf35d0ce
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-03-19 14:37:46 +0800

    REFACTOR: Refactor the web route

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,17 @@ Route::get('/address/barangays', [AddressController::class, 'barangays'])->name(
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         $user = Auth::user();
-        if ($user->hasRole('admin')) return redirect()->route('admin.dashboard');
+        if ($user->hasRole('admin')) {
+            return redirect()->route('admin.dashboard');
+        }
 
-        if ($user->hasRole('dealer') && $user->dealerProfile?->is_approved === true) return redirect()->route('dealer.marketplace.index');
+        if ($user->hasRole('dealer') && $user->dealerProfile?->is_approved === true) {
+            return redirect()->route('dealer.marketplace.index');
+        }
 
-        if ($user->hasRole('farmer') && $user->farmerProfile?->is_approved === true) return redirect()->route('farmer.marketplace.index');
+        if ($user->hasRole('farmer') && $user->farmerProfile?->is_approved === true) {
+            return redirect()->route('farmer.marketplace.index');
+        }
 
         return Inertia::render('Welcome');
     })->name('dashboard');
@@ -42,16 +48,19 @@ if (app()->environment('local', 'development')) {
     Route::prefix('dev')->name('dev.')->group(function () {
         Route::get('login/admin', function () {
             Auth::loginUsingId(1);
+
             return redirect()->route('admin.dashboard');
         })->name('login.admin');
 
         Route::get('login/farmer', function () {
             Auth::loginUsingId(2);
+
             return redirect()->route('farmer.supplies.index');
         })->name('login.farmer');
 
         Route::get('login/dealer', function () {
             Auth::loginUsingId(3);
+
             return redirect()->route('dealer.marketplace.index');
         })->name('login.dealer');
     });
