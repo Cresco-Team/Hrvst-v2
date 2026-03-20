ced9e2a2ec408d84a8e90058b05c4c1c1d37da6d
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-03-20 18:37:50 +0800

    FEAT: Add hearts_count and is_hearted data props to supply and demand resources

--- app/Http/Resources/Marketplace/FarmerSupplyResource.php
+++ app/Http/Resources/Marketplace/FarmerSupplyResource.php
@@ -11,23 +11,24 @@ class FarmerSupplyResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'quantity_kg' => (float) $this->quantity_kg,
+            'offered_price' => (float) $this->offered_price,
+            'price_flag' => $this->price_flag,
+            'status' => $this->status,
             'scheduled_date' => $this->scheduled_date?->format('M d, Y'),
             'time_slot' => $this->time_slot?->value,
             'time_slot_label' => $this->time_slot?->label(),
             'days_until_expiration' => $this->scheduled_date
                 ? (int) now()->diffInDays($this->scheduled_date, false)
                 : null,
+            'hearts_count' => $this->hearts_count ?? 0,
+            'is_hearted' => (bool) ($this->is_hearted ?? false),
             'created_at' => $this->created_at->format('M d, Y'),
             'created_at_human' => $this->created_at->diffForHumans(),
 
             /* with('media') */
             'image_url' => $this->whenLoaded('media', fn () => $this->getFirstMediaUrl('post_image')
             ),
-
-            'quantity_kg' => (float) $this->quantity_kg,
-            'offered_price' => (float) $this->offered_price,
-            'price_flag' => $this->price_flag,
-            'status' => $this->status,
 
             /* with('variety.vegetable.category', 'variety.media') */
             'variety' => $this->whenLoaded('variety', function () {
