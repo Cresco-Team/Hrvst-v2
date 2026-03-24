2b3ac9d5973836d29572a25d536ffe8637d180c6
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-03-24 15:58:47 +0800

    REFACTOR: Use 'Archived' instead of 'Expired'

--- app/Console/Commands/ArchiveOldPostsCommand.php
+++ app/Console/Commands/ArchiveOldPostsCommand.php
@@ -8,13 +8,14 @@ use Illuminate\Console\Command;
 class ArchiveOldPostsCommand extends Command
 {
     protected $signature = 'posts:archive';
+
     protected $description = 'Expire old dealer demands and farmer supplies';
 
     public function handle(ArchiveOldPostsAction $archiveOldPost): int
     {
-        $postsExpired = $archiveOldPost();
+        $postsArchived = $archiveOldPost();
 
-        $this->info("Expired {$postsExpired}.");
+        $this->info("Archived {$postsArchived}.");
 
         return self::SUCCESS;
     }
