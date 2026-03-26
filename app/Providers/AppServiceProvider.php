42431dccbfab98b064636b8d09845647590b05f9
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-03-26 21:02:57 +0800

    REFACTOR: Force scheme to be https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
 
@@ -31,6 +32,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        URL::forceScheme('https');
         $this->configureDefaults();
         Model::automaticallyEagerLoadRelationships();
         Gate::policy(FarmerProfile::class, FarmerPolicy::class);
