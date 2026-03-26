6b98fee8c66b240a3c933780deeea998ba210c09
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-03-26 08:43:15 +0800

    FIX: Fix the decimal points for strict postgresql

--- database/migrations/2025_08_14_170935_create_municipalities_table.php
+++ database/migrations/2025_08_14_170935_create_municipalities_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
             $table->foreignId('province_id')->constrained();
 
             $table->string('name');
-            $table->decimal('latitude', 10, 8);
-            $table->decimal('longitude', 10, 8);
+            $table->decimal('latitude', 10, 7);
+            $table->decimal('longitude', 11, 8);
 
             $table->index(['name']);
         });
