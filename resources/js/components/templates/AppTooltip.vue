9594b8639cb38107f512e5b846610ef1837828d2
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-03-29 00:56:47 +0800

    FEAT: Create generic tooltip

---
+++ resources/js/components/templates/AppTooltip.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '../ui/tooltip';
+
+
+interface Props {
+    content?: string
+    side?: 'top' | 'right' | 'bottom' | 'left'
+    align?: 'start' | 'center' | 'end'
+    delayDuration?: number
+}
+
+withDefaults(defineProps < Props > (), {
+    content: '',
+    side: 'top',
+    align: 'center',
+    delayDuration: 200,
+})
+</script>
+
+<template>
+    <TooltipProvider :delay-duration="delayDuration">
+        <Tooltip>
+            <TooltipTrigger as-child>
+                <slot />
+            </TooltipTrigger>
+
+            <TooltipContent :side="side" :align="align">
+                <slot :name="content">
+                    <p class="text-sm">{{ content }}</p>
+                </slot>
+            </TooltipContent>
+        </Tooltip>
+    </TooltipProvider>
+</template>
