d80aadc3f848db7e427e8c7d4e5243da65e0f335
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-03-31 11:03:56 +0800

    REFACTOR:Update users table for PIN auth

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,9 +11,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->nullable()->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('must_change_pin')->default(false);
             $table->string('phone_number')->unique();
             $table->rememberToken();
             $table->timestamps();
@@ -29,7 +30,7 @@ return new class extends Migration
             $table->string('id')->primary();
             $table->foreignId('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable(); 
+            $table->text('user_agent')->nullable();
             $table->longText('payload');
             $table->integer('last_activity')->index();
         });
