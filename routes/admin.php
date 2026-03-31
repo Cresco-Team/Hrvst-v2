3fa0069bc6f772ed0fbb680517a3cf3fde9f7245
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-03-31 13:55:25 +0800

    FEAT: Remove pending/approve/reject, Add user creation

--- routes/admin.php
+++ routes/admin.php
@@ -3,19 +3,32 @@
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\DealerController;
 use App\Http\Controllers\Admin\FarmerController;
+use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\VarietyPriceController;
 use App\Http\Controllers\Admin\VegetableController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified', 'admin'])->prefix('admin')->name('admin.')->group(function () {
 
-    Route::get('/dashboard', DashboardController::class)
-        ->name('dashboard');
+    Route::get('/dashboard', DashboardController::class)->name('dashboard');
+
+    /* ---------- user management ---------- */
+
+    Route::prefix('users')->name('users.')->group(function () {
+        Route::get('/farmers/create', [UserController::class, 'createFarmerForm'])->name('farmers.create');
+        Route::post('/farmers', [UserController::class, 'storeFarmer'])->name('farmers.store');
+
+        Route::get('/dealers/create', [UserController::class, 'createDealerForm'])->name('dealers.create');
+        Route::post('/dealers', [UserController::class, 'storeDealer'])->name('dealers.store');
+
+        Route::post('/{user}/reset-pin', [UserController::class, 'resetPin'])->name('reset-pin');
+    });
+
+    /* ---------- vegetables ---------- */
 
     Route::prefix('vegetables')->name('vegetables.')->group(function () {
         Route::get('/', [VegetableController::class, 'index'])->name('index');
         Route::get('/{variety}/details', [VegetableController::class, 'details'])->name('details');
-        // show must come after the static /details segment to avoid "details" being matched as a variety ID
         Route::get('/{variety}', [VegetableController::class, 'show'])->name('show');
         Route::post('/', [VegetableController::class, 'store'])->name('store');
         Route::put('/{variety}', [VegetableController::class, 'update'])->name('update');
@@ -23,36 +36,29 @@ Route::middleware(['auth', 'verified', 'admin'])->prefix('admin')->name('admin.'
         Route::delete('/{variety}', [VegetableController::class, 'destroy'])->name('destroy');
     });
 
+    /* ---------- farmers ---------- */
+
     Route::prefix('farmers')->name('farmers.')->group(function () {
         Route::get('/', [FarmerController::class, 'index'])->name('index');
+        Route::get('/{farmer}', [FarmerController::class, 'show'])->name('show');
+        Route::delete('/{farmer}', [FarmerController::class, 'destroy'])->name('destroy');
 
         Route::prefix('api')->name('api.')->group(function () {
-            // Map
             Route::get('/markers', [FarmerController::class, 'markers'])->name('markers');
-            Route::get('/pending', [FarmerController::class, 'pending'])->name('pending');
             Route::get('/{farmer}/details', [FarmerController::class, 'details'])->name('details');
         });
-
-        Route::get('/{farmer}', [FarmerController::class, 'show'])->name('show');
-        Route::delete('{farmer}', [FarmerController::class, 'destroy'])->name('destroy');
-
-        Route::post('/{farmer}/approve', [FarmerController::class, 'approve'])->name('approve');
-        Route::post('/{farmer}/reject', [FarmerController::class, 'reject'])->name('reject');
     });
+
+    /* ---------- dealers ---------- */
 
     Route::prefix('dealers')->name('dealers.')->group(function () {
         Route::get('/', [DealerController::class, 'index'])->name('index');
-
-        Route::prefix('api')->name('api.')->group(function () {
-            Route::get('/pending', [DealerController::class, 'pending'])->name('pending');
-            Route::get('/{dealer}/details', [DealerController::class, 'details'])->name('details');
-        });
-
         Route::get('/{dealer}', [DealerController::class, 'show'])->name('show');
         Route::get('/{dealer}/document', [DealerController::class, 'document'])->name('document');
         Route::delete('/{dealer}', [DealerController::class, 'destroy'])->name('destroy');
 
-        Route::post('/{dealer}/approve', [DealerController::class, 'approve'])->name('approve');
-        Route::post('/{dealer}/reject', [DealerController::class, 'reject'])->name('reject');
+        Route::prefix('api')->name('api.')->group(function () {
+            Route::get('/{dealer}/details', [DealerController::class, 'details'])->name('details');
+        });
     });
 });
