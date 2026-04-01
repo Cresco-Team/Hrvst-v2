346e3c280b798687af882dcb554f6aab2b365cb5
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-04-01 21:31:32 +0800

    FEAT: Create user update profile action

---
+++ app/Actions/Fortify/UpdateProfileInformation.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Actions\Fortify;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Laravel\Fortify\Contracts\UpdatesUserProfileInformation;
+
+class UpdateProfileInformation implements UpdatesUserProfileInformation
+{
+    /**
+     * @param  array<string, mixed>  $input
+     */
+    public function update(User $user, array $input): void
+    {
+        Validator::make($input, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => [
+                'nullable',
+                'string',
+                'email',
+                'max:255',
+                Rule::unique('users')->ignore($user->id)->whereNotNull('email'),
+            ],
+        ])->validateWithBag('updateProfileInformation');
+
+        $user->forceFill([
+            'name' => $input['name'],
+            'email' => $input['email'] ?? null,
+        ])->save();
+    }
+}
