ca78b0709ef17b2151e5b678ec19520586eb4a97
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-04-03 20:17:52 +0800

    FIX: Add municipality to fillable

--- app/Models/Address/Barangay.php
+++ app/Models/Address/Barangay.php
@@ -3,10 +3,12 @@
 namespace App\Models\Address;
 
 use App\Models\Profiles\FarmerProfile;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+#[Fillable(['municipality_id', 'name'])]
 class Barangay extends Model
 {
     public $timestamps = false;
@@ -20,7 +22,7 @@ class Barangay extends Model
         return $this->belongsTo(Municipality::class);
     }
 
-    public function farmers (): HasMany
+    public function farmers(): HasMany
     {
         return $this->hasMany(FarmerProfile::class);
     }
