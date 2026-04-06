a3a44642997f0c1652202cf5cdad68464a9e1d0d
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-04-06 12:19:05 +0800

    REFACTOR: Remove the password pagenavigation

--- resources/js/layouts/settings/Layout.vue
+++ resources/js/layouts/settings/Layout.vue
@@ -8,17 +8,12 @@ import { toUrl } from '@/lib/utils'
 import { edit as editAppearance } from '@/routes/appearance'
 import { edit as editProfile } from '@/routes/profile'
 import { show } from '@/routes/two-factor'
-import { edit as editPassword } from '@/routes/user-password'
 import type { NavItem } from '@/types'
 
 const sidebarNavItems: NavItem[] = [
 	{
 		title: 'Profile',
 		href: editProfile(),
-	},
-	{
-		title: 'Password',
-		href: editPassword(),
 	},
 	{
 		title: 'Two-Factor Auth',
