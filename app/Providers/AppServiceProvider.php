b25fa142f051dbfc0209c19eba20b88a5110a6a2
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-04-06 09:49:42 +0800

    REFACTOR: Register vegetable policy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Marketplace\Post;
 use App\Models\Product\Variety;
+use App\Models\Product\Vegetable;
 use App\Models\Profiles\DealerProfile;
 use App\Models\Profiles\FarmerProfile;
 use App\Observers\PostObserver;
@@ -11,6 +12,7 @@ use App\Observers\VarietyObserver;
 use App\Policies\Marketplace\PostPolicy;
 use App\Policies\Profiles\DealerPolicy;
 use App\Policies\Profiles\FarmerPolicy;
+use App\Policies\VegetablePolicy;
 use Carbon\CarbonImmutable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Date;
@@ -39,6 +41,7 @@ class AppServiceProvider extends ServiceProvider
         Model::automaticallyEagerLoadRelationships();
         Gate::policy(FarmerProfile::class, FarmerPolicy::class);
         Gate::policy(DealerProfile::class, DealerPolicy::class);
+        Gate::policy(Vegetable::class, VegetablePolicy::class);
         Gate::policy(Post::class, PostPolicy::class);
         Variety::observe(VarietyObserver::class);
         Post::observe(PostObserver::class);
