cdc6b70cd3101bef7282d7a2961544bbd9b992f6
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-04-06 09:49:22 +0800

    FEAT: Create policy for vegetable operations

---
+++ app/Policies/VegetablePolicy.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+
+class VegetablePolicy
+{
+    public function create(User $user): bool
+    {
+        return $user->hasRole('admin');
+    }
+
+    public function update(User $user): bool
+    {
+        return $user->hasRole('admin');
+    }
+
+    public function delete(User $user): bool
+    {
+        return $user->hasRole('admin');
+    }
+}
