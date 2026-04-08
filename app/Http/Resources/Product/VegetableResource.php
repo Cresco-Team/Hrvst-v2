0e39e03a25363230561f941ed6f596a6bd48983f
Jerdon Litaoen <jdlitaoen@gmail.com>
2026-04-08 08:58:35 +0800

    FEAT: Create resource for vegetable

---
+++ app/Http/Resources/Product/VegetableResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class VegetableResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'is_variety' => false,
+            'category' => $this->whenLoaded('category', fn () => [
+                'id' => $this->category->id,
+                'name' => $this->category->name,
+            ]),
+            'varieties_count' => $this->whenCounted('varieties'),
+            'varieties' => $this->whenLoaded(
+                'varieties',
+                fn () => VarietyResource::collection($this->varieties)->resolve()
+            ),
+        ];
+    }
+}
