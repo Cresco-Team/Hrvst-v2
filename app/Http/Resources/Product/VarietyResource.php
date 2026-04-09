a6cde19a51d81dfec1989b1713d985eadd799744
Jerdon07 <jdlitaoen@gmail.com>
2026-04-10 07:22:01 +0800

    REFACTOR: Separate variety's detail resource

--- app/Http/Resources/Product/VarietyResource.php
+++ app/Http/Resources/Product/VarietyResource.php
@@ -10,14 +10,12 @@ class VarietyResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            /* Always present */
             'id' => $this->id,
             'name' => $this->name,
             'image_url' => $this->getFirstMediaUrl('variety_image'),
             'hearts_count' => $this->hearts_count,
             'is_hearted' => (bool) ($this->is_hearted ?? false),
 
-            /* Automatically available via $with in Model */
             'vegetable' => [
                 'id' => $this->vegetable->id,
                 'name' => $this->vegetable->name,
@@ -27,20 +25,13 @@ class VarietyResource extends JsonResource
                 ],
             ],
 
-            /* with('latestPrice) */
-            'latest_price' => $this->whenLoaded('latestPrice', function () use ($request) {
-                return $this->latestPrice
-                    ? (new PriceHistoryResource($this->latestPrice))->toArray($request)
-                    : null;
-            }),
-
-            /* Computed when latestPrice is loaded */
-            'price_updated_human' => $this->whenLoaded('latestPrice', fn () => $this->latestPrice?->recorded_at->diffForHumans()
+            'latest_price' => $this->whenLoaded('latestPrice', fn () => $this->latestPrice
+                ? (new PriceHistoryResource($this->latestPrice))->toArray($request)
+                : null
             ),
-            'price_updated_date' => $this->whenLoaded('latestPrice', fn () => $this->latestPrice?->recorded_at->format('M d, Y')
-            ),
+            'price_updated_human' => $this->whenLoaded('latestPrice', fn () => $this->latestPrice?->recorded_at->diffForHumans()),
+            'price_updated_date' => $this->whenLoaded('latestPrice', fn () => $this->latestPrice?->recorded_at->format('M d, Y')),
 
-            /* with('lastTwoPrices) */
             'price_trend' => $this->whenLoaded('lastTwoPrices', function () {
                 $prices = $this->lastTwoPrices;
 
@@ -58,54 +49,8 @@ class VarietyResource extends JsonResource
                 };
             }),
 
-            /* with('recentPrices) */
-            'recent_prices' => $this->whenLoaded('recentPrices', function () use ($request) {
-                return $this->recentPrices
-                    ->sortBy('recorded_at')
-                    ->values()
-                    ->map(fn ($price) => (new PriceHistoryResource($price))->toArray($request))
-                    ->all();
-            }),
-
-            /* withCount([...]) */
             'supply_count' => $this->whenCounted('supply_count'),
             'demand_count' => $this->whenCounted('demand_count'),
-
-            'monthly_supply_kg' => $this->when(
-                isset($this->resource->monthly_supply_kg),
-                fn () => (float) ($this->monthly_supply_kg ?? 0)
-            ),
-
-            'monthly_demand_kg' => $this->when(
-                isset($this->resource->monthly_demand_kg),
-                fn () => (float) ($this->monthly_demand_kg ?? 0)
-            ),
-
-            /* Admin paginated only */
-            'supply_municipalities' => $this->when(
-                isset($this->resource->supply_municipalities),
-                fn () => $this->supply_municipalities
-            ),
-
-            'monthly_activity' => $this->when(
-                isset($this->resource->monthly_activity),
-                fn () => $this->monthly_activity
-            ),
-
-            'quantity_stats' => $this->when(
-                isset($this->resource->quantity_stats),
-                fn () => $this->quantity_stats
-            ),
-
-            'variety_calendar' => $this->when(
-                isset($this->resource->variety_calendar),
-                fn () => $this->variety_calendar
-            ),
-
-            'calendar_summary' => $this->when(
-                isset($this->resource->calendar_summary),
-                fn () => $this->calendar_summary
-            ),
         ];
     }
 }
