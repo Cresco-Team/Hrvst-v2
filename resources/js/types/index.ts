a64ba17453ad9b14c8dc9986427d968583f04786
Jerdon07 <jdlitaoen@gmail.com>
2026-04-17 08:03:29 +0800

    REFACTOR: export the new pages shared type file

--- resources/js/types/index.ts
+++ resources/js/types/index.ts
@@ -4,6 +4,7 @@ export * from './navigation'
 export * from './pages/admin'
 export * from './pages/dealer'
 export * from './pages/farmer'
+export * from './pages/shared'
 export * from './resources/marketplace'
 export * from './resources/product'
 export * from './resources/profile'
@@ -12,7 +13,7 @@ export * from './shared'
 export * from './ui'
 
 import type { Auth } from './auth'
-import { FlashMessage } from './shared'
+import type { FlashMessage } from './shared'
 
 export type AppPageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
 	name: string
