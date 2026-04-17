ef61ab01c5cf7a62d50ae2c432f3de3d0b73a2ec
Jerdon07 <jdlitaoen@gmail.com>
2026-04-17 10:14:36 +0800

    FEAT: Add category vegetables route

--- routes/shared.php
+++ routes/shared.php
@@ -1,10 +1,15 @@
 <?php
 
 use App\Http\Controllers\Shared\CategoryController;
+use App\Http\Controllers\Shared\VegetableController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['can:not-admin'])->group(function () {
     Route::prefix('categories')->name('categories.')->group(function () {
         Route::get('/', [CategoryController::class, 'index'])->name('index');
+
+        Route::prefix('{category}')->name('vegetables.')->group(function () {
+            Route::get('/', [VegetableController::class, 'index'])->name('index');
+        });
     });
 });
