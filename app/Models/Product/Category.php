8d816a7da6d6716ad1c2196c57394c1ae3e3da1a
Jerdon07 <jdlitaoen@gmail.com>
2026-04-17 10:13:16 +0800

    FEAT: Add slug column for URL route binding

--- app/Models/Product/Category.php
+++ app/Models/Product/Category.php
@@ -5,6 +5,7 @@ namespace App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
@@ -13,6 +14,17 @@ class Category extends Model
     protected $fillable = ['name'];
 
     public $timestamps = false;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::saving(function ($category) {
+            if (empty($category->slug)) {
+                $category->slug = Str::slug($category->name);
+            }
+        });
+    }
 
     /* ---------- relations ---------- */
 
