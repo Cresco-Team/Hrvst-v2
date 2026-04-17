9210dccc4c760d9b13fb1a26a32c1c36704f1c88
Jerdon07 <jdlitaoen@gmail.com>
2026-04-17 08:02:31 +0800

    REFACTOR: Use name categories instead of vegetables

--- routes/shared.php
+++ routes/shared.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Shared\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['can:not-admin'])->group(function () {
-    Route::prefix('categories')->name('vegetables.')->group(function () {
+    Route::prefix('categories')->name('categories.')->group(function () {
         Route::get('/', [CategoryController::class, 'index'])->name('index');
     });
 });
