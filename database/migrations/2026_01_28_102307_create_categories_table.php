789f38c22b6a54a66af31bb4d7f2997d4a7781aa
Jerdon07 <jdlitaoen@gmail.com>
2026-04-18 03:02:31 +0800

    FEAT: Index the slug for faster query

--- database/migrations/2026_01_28_102307_create_categories_table.php
+++ database/migrations/2026_01_28_102307_create_categories_table.php
@@ -12,6 +12,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('slug')->unique();
+
+            $table->index(['slug']);
         });
     }
 
