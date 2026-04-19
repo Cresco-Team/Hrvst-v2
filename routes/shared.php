31a63a388a5a3d192d76089d30f3bd08500b3895
Jerdon07 <jdlitaoen@gmail.com>
2026-04-19 23:15:05 +0800

    FEAT: Add vegetable routes

--- routes/shared.php
+++ routes/shared.php
@@ -4,13 +4,11 @@ use App\Http\Controllers\Shared\CategoryController;
 use App\Http\Controllers\Shared\VegetableController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['can:not-admin'])->group(function () {
-    Route::prefix('categories')->name('categories.')->group(function () {
-        Route::get('/', [CategoryController::class, 'index'])->name('index');
+Route::middleware(['can:not-admin'])->prefix('categories')->name('categories.')->group(function () {
+    Route::get('/', [CategoryController::class, 'index'])->name('index');
 
-        Route::prefix('{category}')->name('vegetables.')->group(function () {
-            Route::get('/', [VegetableController::class, 'index'])->name('index');
-            Route::get('/{variety}', [VegetableController::class, 'show'])->name('show');
-        });
+    Route::prefix('{category}')->name('vegetables.')->group(function () {
+        Route::get('/', [VegetableController::class, 'index'])->name('index');
+        Route::get('/{variety}', [VegetableController::class, 'show'])->name('show');
     });
 });
