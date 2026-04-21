bffd119571026156218dc659cc1016cffc3f2bf2
Jerdon07 <jdlitaoen@gmail.com>
2026-04-21 21:54:13 +0800

    REFACTOR: Modify the redirect's route name

--- app/Http/Controllers/Admin/Vegetable/VarietyPriceController.php
+++ app/Http/Controllers/Admin/Vegetable/VarietyPriceController.php
@@ -18,7 +18,7 @@ class VarietyPriceController extends Controller
             priceMax: (float) $request->validated('price_max'),
         );
 
-        return redirect()->route('admin.categories.vegetables.varieties.index')
+        return redirect()->route('admin.vegetables.index')
             ->with('flash', ['type' => 'success', 'message' => 'Price updated successfully.']);
     }
 }
