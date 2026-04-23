49abc7b26123705a5eaed7b5e15ba6138a0a5f6a
Jerdon07 <jdlitaoen@gmail.com>
2026-04-23 17:38:23 +0800

    REFACTOR: Simplify category routes and consolidate with vegetable routes

--- routes/shared.php
+++ routes/shared.php
@@ -1,13 +1,12 @@
 <?php
 
-use App\Http\Controllers\Shared\CategoryController;
 use App\Http\Controllers\Shared\VegetableController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['can:not-admin'])->prefix('categories')->name('categories.')->group(function () {
-    Route::get('/', [CategoryController::class, 'index'])->name('index');
+Route::middleware(['can:not-admin'])->group(function () {
+    Route::get('/categories', [VegetableController::class, 'category'])->name('categories');
 
-    Route::prefix('{category}')->name('vegetables.')->group(function () {
+    Route::prefix('vegetables')->name('vegetables.')->group(function () {
         Route::get('/', [VegetableController::class, 'index'])->name('index');
         Route::get('/{variety}', [VegetableController::class, 'show'])->name('show');
     });
