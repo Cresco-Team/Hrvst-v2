5191c013ea3590eb5ec04eb10f735c2eb451a97e
Jerdon07 <jdlitaoen@gmail.com>
2026-04-24 20:48:25 +0800

    FEAT: Add farmer dashboard route

--- routes/farmer.php
+++ routes/farmer.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\Farmer\DashboardController;
 use App\Http\Controllers\Farmer\MarketplaceController;
 use App\Http\Controllers\Farmer\SupplyController;
 use App\Http\Controllers\Farmer\SupplyMapController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified', 'farmer'])->prefix('farmer')->name('farmer.')->group(function () {
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
     Route::prefix('supplies')->name('supplies.')->group(function () {
         Route::get('/', [SupplyController::class, 'index'])->name('index');
