c5ddd0511b7e8ab6fe4aef63e16c29833817c168
Jerdon07 <jdlitaoen@gmail.com>
2026-04-24 19:08:09 +0800

    FEAT: Create useCapitalize for string manipulation

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -9,3 +9,7 @@ export function cn(...inputs: ClassValue[]) {
 export function toUrl(href: NonNullable<InertiaLinkProps['href']>) {
 	return typeof href === 'string' ? href : href?.url
 }
+
+export const useCapitalize = (text: string): string => {
+  return text.charAt(0).toUpperCase() + text.slice(1);
+}
