97c37ed471e2e6594463afbbfb59f6002faa2861
Jerdon07 <jdlitaoen@gmail.com>
2026-05-07 08:07:21 +0800

    FIX: Update PostStatus type to remove 'ongoing' and change 'harvested' definition

--- resources/js/types/enums.ts
+++ resources/js/types/enums.ts
@@ -1,4 +1,5 @@
-export type PostStatus = 'growing' | 'ongoing' | 'archived' | 'fulfilled'
+export type PostStatus = 'growing' | 'harvested'
+export type PostItemStatus = 'ongoing' | 'fulfilled' | 'archived'
 export type PostType = 'supply' | 'demand'
 export type PostTimeSlot = 'morning' | 'afternoon' | 'evening'
 export type PostPriceFlag = 'Low' | 'Fair' | 'High'
