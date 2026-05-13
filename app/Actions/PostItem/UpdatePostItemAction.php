4daf6c0e2d03bf54f62d4e62848a52e55f728cb6
Jerdon07 <jdlitaoen@gmail.com>
2026-05-13 21:52:12 +0800

    FEAT: Implement UpdatePostItemAction to handle post item updates

---
+++ app/Actions/PostItem/UpdatePostItemAction.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Actions\PostItem;
+
+use App\Models\Marketplace\PostItem;
+
+class UpdatePostItemAction
+{
+    public function handle(PostItem $postItem, array $validated): void
+    {
+        $postItem->update([
+            'variety_id' => $validated['variety_id'],
+            'quantity_kg' => $validated['quantity_kg'],
+            'unit_price' => $validated['unit_price'] ?? null,
+        ]);
+    }
+}
