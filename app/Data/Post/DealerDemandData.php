86879d56e0ac6f8eeb75338b6714de2d7e5d6cdd
Jerdon07 <jdlitaoen@gmail.com>
2026-06-25 00:11:15 +0800

    FEATURE: Add DealerDemandData class for managing dealer demand data structure

---
+++ app/Data/Post/DealerDemandData.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Data\Post;
+
+use App\Data\PostItem\PostItemLightData;
+use App\Enums\PostTimeSlot;
+use App\Enums\PostType;
+use Carbon\Carbon;
+use Spatie\LaravelData\Attributes\DataCollectionOf;
+use Spatie\LaravelData\Attributes\WithCast;
+use Spatie\LaravelData\Attributes\WithTransformer;
+use Spatie\LaravelData\Casts\DateTimeInterfaceCast;
+use Spatie\LaravelData\Data;
+use Spatie\LaravelData\DataCollection;
+use Spatie\LaravelData\Transformers\DateTimeInterfaceTransformer;
+use Spatie\TypeScriptTransformer\Attributes\TypeScript;
+use Spatie\TypeScriptTransformer\Attributes\TypeScriptType;
+
+#[TypeScript]
+class DealerDemandData extends Data
+{
+    public function __construct(
+        public int $id,
+        public int $user_id,
+        public PostType $type,
+        #[WithCast(DateTimeInterfaceCast::class, format: 'F j, Y')]
+        #[WithTransformer(DateTimeInterfaceTransformer::class, format: 'F j, Y')]
+        #[TypeScriptType('string')]
+        public Carbon $scheduled_date,
+        public PostTimeSlot $time_slot,
+        public string $created_at,
+        public string $created_at_human,
+
+        /** @var DataCollection<int, PostItemLightData> */
+        #[DataCollectionOf(PostItemLightData::class)]
+        public ?DataCollection $post_items,
+    ) {}
+}
