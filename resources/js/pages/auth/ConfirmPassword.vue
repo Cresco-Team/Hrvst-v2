e62826a54a9ae9c235fd445af4cf151245281648
Jerdon07 <jdlitaoen@gmail.com>
2026-06-30 06:35:52 +0800

    FEAT: ConfirmPassword.vue with 6-slot InputOTP

--- resources/js/pages/auth/ConfirmPassword.vue
+++ resources/js/pages/auth/ConfirmPassword.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { Form, Head } from '@inertiajs/vue3'
 import InputError from '@/components/InputError.vue'
 import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
+import { InputOTP, InputOTPGroup, InputOTPSlot } from '@/components/ui/input-otp'
 import { Label } from '@/components/ui/label'
 import { Spinner } from '@/components/ui/spinner'
 import AuthLayout from '@/layouts/AuthLayout.vue'
+
+const pin = ref('')
 </script>
 
 <template>
@@ -18,23 +21,28 @@ import AuthLayout from '@/layouts/AuthLayout.vue'
         <Form
             action="/user/confirm-password"
             method="post"
+            reset-on-error
+            @error="pin = ''"
             v-slot="{ errors, processing }"
             class="flex flex-col gap-6"
         >
+            <input type="hidden" name="password" :value="pin" />
+
             <div class="grid gap-2">
-                <Label for="password">PIN</Label>
-                <Input
-                    id="password"
-                    type="password"
-                    name="password"
-                    required
-                    autofocus
-                    :tabindex="1"
-                    autocomplete="current-password"
-                    placeholder="••••"
-                    inputmode="numeric"
-                    maxlength="4"
-                />
+                <Label for="pin">PIN</Label>
+                <div class="flex justify-center">
+                    <InputOTP
+                        id="pin"
+                        v-model="pin"
+                        :maxlength="6"
+                        :disabled="processing"
+                        autofocus
+                    >
+                        <InputOTPGroup>
+                            <InputOTPSlot v-for="index in 6" :key="index" :index="index - 1" />
+                        </InputOTPGroup>
+                    </InputOTP>
+                </div>
                 <InputError :message="errors.password" />
             </div>
 
