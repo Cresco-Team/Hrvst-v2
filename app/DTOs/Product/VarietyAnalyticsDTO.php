22c392fe135543004c7bd5e2ef6c1239df79b46b
Jerdon07 <jdlitaoen@gmail.com>
2026-07-03 06:13:56 +0800

    REFACTOR: Update VarietyAnalyticsDTO and VegetableData for TypeScript compatibility

--- app/DTOs/Product/VarietyAnalyticsDTO.php
+++ app/DTOs/Product/VarietyAnalyticsDTO.php
@@ -3,7 +3,9 @@
 namespace App\DTOs\Product;
 
 use App\Enums\Analytics\ImbalanceBand;
+use Spatie\TypeScriptTransformer\Attributes\TypeScript;
 
+#[TypeScript]
 class VarietyAnalyticsDTO
 {
     public function __construct(
