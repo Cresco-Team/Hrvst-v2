c1929b8f8e32c9cd736ac47047c77cc59ad09b20
Jerdon07 <jdlitaoen@gmail.com>
2026-07-03 13:18:37 +0800

    REFACTOR: Modify display name to include the local_name

--- app/Models/Product/Vegetable.php
+++ app/Models/Product/Vegetable.php
@@ -50,9 +50,13 @@ class Vegetable extends Model implements HasMedia
     public function displayName(): Attribute
     {
         return Attribute::make(
-            get: fn () => $this->variety_name
-                ? "{$this->vegetable_name}: {$this->variety_name}"
-                : $this->vegetable_name,
+            get: function () {
+                $local = $this->local_name ? " ({$this->local_name})" : '';
+                
+                return $this->variety_name
+                    ? "{$this->vegetable_name}: {$this->variety_name}{$local}"
+                    : "{$this->vegetable_name}{$local}";
+            }
         );
     }
 
