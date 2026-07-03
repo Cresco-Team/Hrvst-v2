ceb157ffb3fd6badb3d92a94b770c162ed9408a9
Jerdon07 <jdlitaoen@gmail.com>
2026-07-03 15:56:55 +0800

    REFACTOR: Modify to remove unnecessary bottom space

--- resources/js/components/Heading.vue
+++ resources/js/components/Heading.vue
@@ -11,7 +11,7 @@ withDefaults(defineProps<Props>(), {
 </script>
 
 <template>
-    <header :class="variant === 'small' ? '' : 'mb-8 space-y-0.5'">
+    <header>
         <h1
             :class="
                 variant === 'small'
