d9c1997c3145cec7464bf16d5958ddbf8d3a43c4
Jerdon07 <jdlitaoen@gmail.com>
2026-07-04 17:36:43 +0800

    FEAT: Implement onboarding guide composable

---
+++ resources/js/composables/useOnboardingGuide.ts
@@ -0,0 +1,15 @@
+import { ref } from 'vue'
+
+const isOpen = ref(false)
+
+export function useOnboardingGuide() {
+	const open = (): void => {
+		isOpen.value = true
+	}
+
+	const close = (): void => {
+		isOpen.value = false
+	}
+
+	return { isOpen, open, close }
+}
