a192b1087d39ed064c57f334737fb67c575cabf0
Jerdon07 <jdlitaoen@gmail.com>
2026-07-05 17:53:49 +0800

    FEAT: Update routes to use PostItemController for item fulfillment and expiration

--- routes/dealer.php
+++ routes/dealer.php
@@ -8,18 +8,17 @@ Route::middleware(['auth', 'verified', 'dealer'])->prefix('dealer')->name('deale
 
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-    Route::prefix('dashboard')->name('dashboard.')->group(function () {
-        Route::prefix('items')->name('items.')->group(function () {
-            Route::post('/{postItem}/fulfill', [DashboardController::class, 'fulfillItem'])->name('fulfill');
-            Route::post('/{postItem}/expire', [DashboardController::class, 'expireItem'])->name('expire');
-        });
-    });
-
     Route::prefix('demands')->name('demands.')->group(function () {
         Route::get('/', [DemandController::class, 'index'])->name('index');
         Route::get('/archived', [DemandController::class, 'archived'])->name('archived'); // must be before /{demand}
         Route::post('/', [DemandController::class, 'store'])->name('store');
         Route::put('/{demand}', [DemandController::class, 'update'])->name('update');
         Route::delete('/{demand}', [DemandController::class, 'destroy'])->name('destroy');
+
+        Route::prefix('items')->name('items.')->group(function () {
+            Route::post('/{postItem}/fulfill', [PostItemController::class, 'fulfill'])->name('fulfill');
+            Route::post('/{postItem}/expire', [PostItemController::class, 'expire'])->name('expire');
+            Route::delete('/{postItem}', [PostItemController::class, 'destroy'])->name('destroy');
+        });
     });
 });
