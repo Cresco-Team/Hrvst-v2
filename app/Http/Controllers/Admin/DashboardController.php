9298faab124e65ff2b9d0297d321c29ca7ede2d5
Jerdon07 <jdlitaoen@gmail.com>
2026-07-11 17:29:44 +0800

    FEAT: Integrate RegistrationTrendService into DashboardController for monthly registration trends

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,15 +4,19 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Services\Admin\DashboardService;
+use App\Services\Admin\RegistrationTrendService;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class DashboardController extends Controller
 {
-    public function __invoke(DashboardService $dashboardService): Response
-    {
+    public function __invoke(
+        DashboardService $dashboardService,
+        RegistrationTrendService $registrationTrendService,
+    ): Response {
         return Inertia::render('admin/Dashboard', [
             'kpis' => Inertia::defer(fn () => $dashboardService->getKPIs()),
+            'registrationTrends' => Inertia::defer(fn () => $registrationTrendService->monthly()),
         ]);
     }
 }
