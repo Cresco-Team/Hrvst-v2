bde83af0e61dda576742fe5f3dab9fc17b11558b
Jerdon07 <jdlitaoen@gmail.com>
2026-07-11 14:23:31 +0800

    FEAT: Add TopVegetableData class for vegetable profile data handling

---
+++ app/Data/Profile/TopVegetableData.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Data\Profile;
+
+use Spatie\LaravelData\Data;
+use Spatie\TypeScriptTransformer\Attributes\TypeScript;
+
+#[TypeScript]
+class TopVegetableData extends Data
+{
+    public function __construct(
+        public int $vegetable_id,
+        public string $display_name,
+        public string $image_url,
+        public int $post_count,
+        public float $total_kg,
+    ) {}
+}
