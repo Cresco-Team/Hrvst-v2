e06f09e98aca3f0fe504f992c16eccf882a4c6d3
Jerdon07 <jdlitaoen@gmail.com>
2026-07-12 17:32:40 +0800

    FEAT: Add EnsureSubscriptionActive middleware to enforce active subscriptions

---
+++ app/Http/Middleware/EnsureSubscriptionActive.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Enums\Billing\SubscriptionFeature;
+use App\Models\Billing\Subscription;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureSubscriptionActive
+{
+    public function handle(Request $request, Closure $next, string $feature): Response
+    {
+        $feature = SubscriptionFeature::from($feature);
+
+        if (! Subscription::hasAccess($request->user(), $feature)) {
+            return redirect()->route('billing.show')->with('flash', [
+                    'type' => 'warning',
+                    'message' => 'This feature requires an active subscription.',
+                ]);
+        }
+
+        return $next($request);
+    }
+}
