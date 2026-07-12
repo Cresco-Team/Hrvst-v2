85130b46822cd4f5bd6b35435073623be5e13b73
Jerdon07 <jdlitaoen@gmail.com>
2026-07-12 21:49:19 +0800

    REFACTOR: Update type interface

--- resources/js/components/shared/charts/WasteRankingCard.vue
+++ resources/js/components/shared/charts/WasteRankingCard.vue
@@ -9,9 +9,9 @@ import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { show as adminShow } from '@/routes/admin/vegetables'
 import { show as sharedShow } from '@/routes/vegetables'
-import type { VegetableStabilityData, VegetableWasteData } from '@/types/resources/product'
+import type { TopVegetableData, VegetableStabilityData, VegetableWasteData } from '@/types/resources/product'
 
-type RankedItem = VegetableWasteData | VegetableStabilityData
+type RankedItem = VegetableWasteData | VegetableStabilityData | TopVegetableData
 
 const props = defineProps<{
     title: string
