ea4431f38ed11a263c0e08199f3039fdf0036eef
Jerdon07 <jdlitaoen@gmail.com>
2026-07-24 12:43:02 +0800

    REFACTOR: Remove unused request parameter

--- app/Http/Controllers/Farmer/PostItemController.php
+++ app/Http/Controllers/Farmer/PostItemController.php
@@ -8,12 +8,11 @@ use App\Enums\PostType;
 use App\Http\Controllers\Controller;
 use App\Models\Marketplace\PostItem;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 
 class PostItemController extends Controller
 {
-    public function fulfill(Request $request, PostItem $postItem, FulfillPostItemAction $action): RedirectResponse
+    public function fulfill(PostItem $postItem, FulfillPostItemAction $action): RedirectResponse
     {
         $postItem->load('post');
         Gate::authorize('fulfill', $postItem);
@@ -25,7 +24,7 @@ class PostItemController extends Controller
             ->with('flash', ['type' => 'success', 'message' => 'Item marked as fulfilled.']);
     }
 
-    public function expire(Request $request, PostItem $postItem, ExpirePostItemAction $action): RedirectResponse
+    public function expire(PostItem $postItem, ExpirePostItemAction $action): RedirectResponse
     {
         $postItem->load('post');
         Gate::authorize('expire', $postItem);
@@ -37,7 +36,7 @@ class PostItemController extends Controller
             ->with('flash', ['type' => 'success', 'message' => 'Item marked as expired.']);
     }
 
-    public function destroy(Request $request, PostItem $postItem): RedirectResponse
+    public function destroy(PostItem $postItem): RedirectResponse
     {
         $postItem->load('post');
         Gate::authorize('delete', $postItem);
