19065f74cd1da016a099e1bb349a073ffc1e307b
Jerdon07 <jdlitaoen@gmail.com>
2026-08-16 19:27:21 +0800

    FEAT: Add 'Password' link to settings sidebar navigation

--- resources/js/layouts/settings/Layout.vue
+++ resources/js/layouts/settings/Layout.vue
@@ -9,6 +9,7 @@ import { edit as editAppearance } from '@/routes/appearance'
 import { edit as editProfile } from '@/routes/profile'
 import { show } from '@/routes/two-factor'
 import type { NavItem } from '@/types'
+import password from '@/routes/password'
 
 const sidebarNavItems: NavItem[] = [
 	{
@@ -19,6 +20,10 @@ const sidebarNavItems: NavItem[] = [
 		title: 'Two-Factor Auth',
 		href: show(),
 	},
+    {
+        title: 'Password',
+        href: password.edit(),
+    },
 	{
 		title: 'Appearance',
 		href: editAppearance(),
