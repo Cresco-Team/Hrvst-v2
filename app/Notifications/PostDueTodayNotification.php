9ea8c3202a183941cb217a8dd41fb2f4c63a1569
Jerdon07 <jdlitaoen@gmail.com>
2026-08-26 19:02:37 +0800

    FEAT: Create PostDueTodayNotification class for web push notifications

---
+++ app/Notifications/PostDueTodayNotification.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Enums\PostType;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use NotificationChannels\WebPush\WebPushChannel;
+use NotificationChannels\WebPush\WebPushMessage;
+
+class PostDueTodayNotification extends Notification
+{
+    use Queueable;
+
+    public function __construct() {}
+
+    /**
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database', WebPushChannel::class];
+    }
+
+    public function toWebPush(object $notifiable, self $notification): WebPushMessage
+    {
+        $routeName = $this->post->type === PostType::Supply
+            ? 'farmer.supplies.index'
+            : 'dealer.demands.index';
+ 
+        return (new WebPushMessage)
+            ->title('Schedule due today')
+            ->icon('/icons/pwa-192x192.png')
+            ->body(sprintf(
+                'Your %s for %s is due today.',
+                $this->post->type->value,
+                $this->post->scheduled_date->format('M j'),
+            ))
+            ->data(['url' => route($routeName)])
+            ->options(['TTL' => 3600]);
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'post_id' => $this->post->id,
+            'type' => $this->post->type->value,
+            'scheduled_date' => $this->post->scheduled_date->toDateString(),
+        ];
+    }
+}
