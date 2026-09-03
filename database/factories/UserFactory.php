e250685e9f7a5e1546a02399be301408e02c728b
Jerdon07 <jdlitaoen@gmail.com>
2026-09-03 10:28:49 +0800

    FIX: change fake safeEmail() to freeEmail()

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,7 +18,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'email' => fake()->unique()->freeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('0000'),
             'phone_number' => '09'.fake()->numerify('#########'),
